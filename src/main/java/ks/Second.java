dc58a44f38ea2913beddd2f5aaa275a918899c23
arjunsq <arjunsoman@qburst.com>
2018-07-23 14:36:29 +0530

    Another

---
+++ src/main/java/ks/Second.java
@@ -0,0 +1,14 @@
+package ks;
+
+import arjun.*;
+
+public class Second
+{
+	public static void main ( String args[] )
+	{
+		App s = new App();
+		s.first();
+		System.out.println("This is second program's code");
+	}
+}
+
